7a1fac4fd5a9c0810976846e8a139e87eb6a6d6a
Isaac Avram <avrisaac555@gmail.com>
2019-01-26 15:22:50 -0800

    Added hash.h

---
+++ Hash Study/hash.h
@@ -0,0 +1,33 @@
+#include <cstddef>
+
+/**
+ * A hashing algorithm based on a Linear Congruential Generator
+ */
+class LCRHash
+{
+private:
+    const unsigned long modulus;
+    const unsigned long multiplier;
+    const unsigned long increment;
+
+public:
+    explicit LCRHash(unsigned long modulus      = 2147483648,
+                     unsigned long multiplier   = 1103515245,
+                     unsigned long increment    = 12345) :
+                     modulus(modulus),
+                     multiplier(multiplier),
+                     increment(increment) {}
+
+    unsigned long hash(unsigned short* data, std::size_t size)
+    {
+        unsigned long rv = 0;
+
+        for(std::size_t i = 0; i < size; i++)
+        {
+            unsigned long seed = (rv << 32) | data[i];
+            rv = (multiplier * seed + increment) % modulus;
+        }
+
+        return rv;
+    }
+};
