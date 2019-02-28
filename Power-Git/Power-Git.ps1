d4176c9742e12bc7e3c688b200caac10107140b5
Isaac Avram <avrisaac555@gmail.com>
2019-02-27 21:24:45 -0800

    Added some more git commands

--- Power-Git/Power-Git.ps1
+++ Power-Git/Power-Git.ps1
@@ -1,4 +1,4 @@
-enum GitFileStatus {
+enum GitFileStatusAttribute {
     unmodified
     modified
     added
@@ -10,26 +10,28 @@ enum GitFileStatus {
     ignored
 }
 
-function Convert-StringToGitFileStatus ($c){
+function Convert-StringToGitFileStatusAttribute ($c){
     <#
     .SYNOPSIS
-        Converts a string to a GitFileStatus
+        Converts a string to a GitFileStatusAttribute
     #>
     switch ($c) {
-        ' ' {[GitFileStatus]::unmodified}
-        'M' {[GitFileStatus]::modified}
-        'A' {[GitFileStatus]::added}
-        'D' {[GitFileStatus]::deleted}
-        'R' {[GitFileStatus]::renamed}
-        'C' {[GitFileStatus]::copied}
-        'U' {[GitFileStatus]::updated}
-        '?' {[GitFileStatus]::untracked}
-        '!' {[GitFileStatus]::ignored}
+        ' ' {[GitFileStatusAttribute]::unmodified}
+        'M' {[GitFileStatusAttribute]::modified}
+        'A' {[GitFileStatusAttribute]::added}
+        'D' {[GitFileStatusAttribute]::deleted}
+        'R' {[GitFileStatusAttribute]::renamed}
+        'C' {[GitFileStatusAttribute]::copied}
+        'U' {[GitFileStatusAttribute]::updated}
+        '?' {[GitFileStatusAttribute]::untracked}
+        '!' {[GitFileStatusAttribute]::ignored}
     }
 }
 
 class GitRepo {
     [ValidateNotNullOrEmpty()][string]$Path
+
+    GitRepo([string]$Path) {$this.Path = $Path}
 
     [string] CurrentBranch() {return git symbolic-ref HEAD}
     [string] ToString() {return "Git:" + $this.Path + ":" + $this.CurrentBranch()}
@@ -39,8 +41,87 @@ class GitRepo {
     [string[]] Tags() {return git rev-parse --symbolic-full-name --tags}
 }
 
+class GitFileStatus {
+    [ValidateNotNullOrEmpty()][GitRepo]$Repo
+    [ValidateNotNullOrEmpty()][string]$FileName
+    [ValidateNotNullOrEmpty()][GitFileStatusAttribute]$IndexStatus
+    [ValidateNotNullOrEmpty()][GitFileStatusAttribute]$WorkTreeStatus
+
+    GitFileStatus([GitRepo]$Repo, [string]$FileName, [GitFileStatusAttribute]$IndexStatus, [GitFileStatusAttribute]$WorkTreeStatus) {
+        $this.Repo = $Repo
+        $this.FileName = $FileName
+        $this.IndexStatus = $IndexStatus
+        $this.WorkTreeStatus = $WorkTreeStatus
+    }
+}
+
 function New-GitBranch {
+    <#
+    .SYNOPSIS
+        Creates a new branch with the name $BranchName in the Git repository specified by $Repo
+    #>
     Param(
-        
+        # The git repository to operate on
+        [GitRepo]
+        $Repo = [GitRepo]::new("."),
+
+        # The name of the new branch
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]
+        $BranchName,
+
+        # If enabled, make the new branch the current one
+        [Switch]
+        $MakeCurrent
     )
+
+    Push-Location $Repo.Path
+
+    if ($MakeCurrent) {
+        git checkout -b $BranchName
+    } else {
+        git branch $BranchName
+    }
+
+    Pop-Location
+    return $Repo
+}
+
+function Set-GitBranch {
+    <#
+    .SYNOPSIS
+        Checkout a branch in the git repository
+    #>
+    Param(
+        # The git repository to operate on
+        [GitRepo]
+        $Repo = [GitRepo]::new("."),
+
+        # The name of the new branch
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]
+        $BranchName
+    )
+
+    Push-Location $Repo.Path
+    git checkout $BranchName
+    Pop-Location
+    return $Repo
+}
+
+function Get-GitFileStatus {
+    Param(
+        [GitRepo]
+        $Repo = [GitRepo]::new(".")
+    )
+
+    Push-Location $Repo.Path
+    $status_output = git status --porcelain
+    Pop-Location
+
+    ForEach-Object -InputObject $status_output {
+        $is = Convert-StringToGitFileStatusAttribute($_.Chars(0))
+        $ts = Convert-StringToGitFileStatusAttribute($_.Chars(1))
+        [GitFileStatus]::new($Repo, $_.Substring(3), $is, $ts)
+    }
 }
